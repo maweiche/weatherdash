547fff06679469b4e77065765fac4e940a698e6e
Matt Weichel <maweiche@gmail.com>
2021-10-01 18:25:24 -0700

    added to js, created element variables and base code to create display

---
+++ script.js
@@ -0,0 +1,32 @@
+var mainWeatherContainerEl = document.getElementById("#mainWeatherContainer");
+var searchContainerEl = document.getElementById("#searchContainer");
+var searchInputEl = document.getElementById("#searchInput");
+var fiveDayContainerEl = document.getElementById("#fiveDayContainer");
+var submitBtn = document.getElementById("#submitButton");
+var day1Card = document.getElementById("#d0Container");
+var day2Card = document.getElementById("#d1Container");
+var day3Card = document.getElementById("#d2Container");
+var day4Card = document.getElementById("#d3Container");
+var day5Card = document.getElementById("#d4Container");
+
+
+function getAPI () {
+    // Make fetch() request then loop through the data, creating new HTML elements
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var dayName = document.createElement('article');
+            var futureWeatherData = document.createElement('div');
+
+            //Edit these for correct APIT tagging...
+            dayName.textContent = data[i].dayName;
+            futureWeatherData.textContent = data[i].weatherdatafor5day;
+            fiveDayContainerEl.append(dayName);
+            fiveDayContainerEl.append(futureWeatherData);
+
+        })
+}
+submitBtn.addEventListener('click', getAPI);
