13029fc2879db465da8b294ebd26ba8911fa21f9
Matt Weichel <maweiche@gmail.com>
2021-10-02 09:37:51 -0700

    corrected submit button issue with linking js

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ var mainWeatherContainerEl = document.getElementById("#mainWeatherContainer");
 var searchContainerEl = document.getElementById("#searchContainer");
 var searchInputEl = document.getElementById("#searchInput");
 var fiveDayContainerEl = document.getElementById("#fiveDayContainer");
-var submitBtn = document.getElementById("#submitButton");
+var submitBtn = document.getElementById('submitButton');
 var day1Card = document.getElementById("#d0Container");
 var day2Card = document.getElementById("#d1Container");
 var day3Card = document.getElementById("#d2Container");
 var day4Card = document.getElementById("#d3Container");
 var day5Card = document.getElementById("#d4Container");
-var cityName = document.getElementById("#searchInput");
 
+
+
+
+function testClick (){
+    console.log("this is a test click");
+}
 
 function getAPI () {
-    var requestURL = 'api.openweathermap.org/data/2.5/forecast?q='+'{city name}'+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
+    var cityName = searchInputEl;
+    var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
     // Make fetch() request then loop through the data, creating new HTML elements
     fetch(requestURL)
         .then(function (response) {
+            console.log("this is the response: ", response);
             return response.json();
+         
         })
         .then(function (data) {
             console.log(data);
@@ -29,6 +37,6 @@ function getAPI () {
             fiveDayContainerEl.append(dayName);
             fiveDayContainerEl.append(futureWeatherData);
 
-        })
+        });
 }
 submitBtn.addEventListener('click', getAPI);
