462a0ae4dce83ce5aea8d96c09ea7c7220be7b13
Matt Weichel <maweiche@gmail.com>
2021-10-02 14:36:47 -0700

    swtiched from submit element to jquery responsive submit and input ajax api linkage

--- script.js
+++ script.js
@@ -9,34 +9,92 @@ var day3Card = document.getElementById("#d2Container");
 var day4Card = document.getElementById("#d3Container");
 var day5Card = document.getElementById("#d4Container");
 
+var cityName = "Raleigh";
+var requestURL5day = 'https://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
+var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
 
 
+$(document).ready(function() {
 
-function testClick (){
-    console.log("this is a test click");
-}
+    var citySearchArray = ["Raleigh"];
 
-function getAPI () {
-    var cityName = searchInputEl;
-    var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
-    // Make fetch() request then loop through the data, creating new HTML elements
-    fetch(requestURL)
-        .then(function (response) {
-            console.log("this is the response: ", response);
-            return response.json();
-         
+    //function to make buttons appear on the page
+    function populateElements(citySearchArray, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (var i = 0; citySearchArray.length; i++) {
+            var newCityButton = $("<button>");
+            newCityButton.addClass(classToAdd);
+            newCityButton.attr("data-type", citySearchArray[i]);
+            newCityButton.text(citySearchArray[i]);
+            $(areaToAddTo).append(newCityButton);
+        }
+    }
+    
+    $(document).on("click", ".city-button", function(event) {
+        $("#cities").empty();
+        $(".city-button").removeClass("active")
+        $(this).addClass("active");
+
+        var type = $(this).attr("data-type");
+        var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
+
+        $.ajax({
+            url: requestURLsolo,
+            method: "GET"
         })
-        .then(function (data) {
-            console.log(data);
-            var dayName = document.createElement('article');
-            var futureWeatherData = document.createElement('div');
+            .then(function(response) {
+                var results = response.data;
 
-            //Edit these for correct APIT tagging...
-            dayName.textContent = data[i].dayName;
-            futureWeatherData.textContent = data[i].weatherdatafor5day;
-            fiveDayContainerEl.append(dayName);
-            fiveDayContainerEl.append(futureWeatherData);
+                for (var i = 0; i < results.length; i++) {
+                    var weatherSoloDiv = $("<div class=\"weather\">")
+                    var cityName = results[i].name;
+                    var temperature = results[i].main.temp;
+                    var windSpeed = results [i].wind.speed;
 
-        });
-}
-submitBtn.addEventListener('click', getAPI);
+                    var p = $("<p>").text(cityName, temperature, windSpeed)
+
+                    weatherSoloDiv.append(p);
+                }
+            })
+    })
+    // add new city button?
+    $("#add-city").on("click", function(event) {
+        event.preventDefault();
+
+        var newCity = $("input").eq(0).val();
+        var newCity = $("#city-input").val();
+
+        if (newCity.length > 2) {
+            citySearchArray.push(newCity)
+        }
+
+        populateElements(citySearchArray, "city-button", "#city-buttons");
+    });
+    populateElements(citySearchArray, "city-button", "#city-buttons");
+})
+
+// function getAPI () {
+//     // console.log(requestURL5day);
+//     // Make fetch() request then loop through the data, creating new HTML elements
+//     fetch(requestURL5day)
+//         .then(function (response) {
+//             return response.json();
+         
+//         })
+//         .then(function (data) {
+//             for (var i = 0; i < data.length; i++) {
+//             var dayName = document.createElement('article');
+//             var futureWeatherData = document.createElement('div');
+
+//             //Edit these for correct APIT tagging...
+//             dayName.textContent = data[i].forecast.time;
+//             console.log(dayName.textContent);
+//             futureWeatherData.textContent = data[i].weatherdatafor5day;
+//             fiveDayContainerEl.append(dayName);
+//             fiveDayContainerEl.append(futureWeatherData);
+//             }        
+//         });
+// }
+// // console.log(getAPI);
+// submitBtn.addEventListener('click', getAPI(requestURL5day));
