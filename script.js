77e7583d337f851747b50e31d6c8f8d60f34da28
Matt Weichel <maweiche@gmail.com>
2021-10-02 19:07:02 -0700

    added function to search card and added clear button in html

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-var mainWeatherContainerEl = document.getElementById("#mainWeatherContainer");
-var searchContainerEl = document.getElementById("#searchContainer");
-var searchInputEl = document.getElementById("#searchInput");
-var fiveDayContainerEl = document.getElementById("#fiveDayContainer");
-var submitBtn = document.getElementById('submitButton');
-var day1Card = document.getElementById("#d0Container");
-var day2Card = document.getElementById("#d1Container");
-var day3Card = document.getElementById("#d2Container");
-var day4Card = document.getElementById("#d3Container");
-var day5Card = document.getElementById("#d4Container");
+var mainWeatherContainerEl = $("#mainWeatherContainer");
+var searchContainerEl = $("#searchContainer");
+var searchInput = $("#searchInput");
+var fiveDayContainerEl = $("#fiveDayContainer");
+var submitBtn = $("#submitButton");
 
-var cityName = "Raleigh";
+var mainCity = $("#main-city");
+
+var cityName = "Raleigh" ;
 var requestURL5day = 'https://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
 var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
 
@@ -22,7 +19,7 @@ $(document).ready(function() {
     function populateElements(citySearchArray, classToAdd, areaToAddTo) {
         $(areaToAddTo).empty();
 
-        for (var i = 0; citySearchArray.length; i++) {
+        for (var i = 0; i < citySearchArray.length; i++) {
             var newCityButton = $("<button>");
             newCityButton.addClass(classToAdd);
             newCityButton.attr("data-type", citySearchArray[i]);
@@ -31,39 +28,44 @@ $(document).ready(function() {
         }
     }
     
-    $(document).on("click", ".city-button", function(event) {
-        $("#cities").empty();
-        $(".city-button").removeClass("active")
-        $(this).addClass("active");
+    // $(document).on("click", ".city-button", function(event) {
+    //     $("#cities").empty();
+    //     $(".city-button").removeClass("active")
+    //     $(this).addClass("active");
 
-        var type = $(this).attr("data-type");
-        var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
+    //     var type = $(this).attr("data-type");
+    //     var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
 
-        $.ajax({
-            url: requestURLsolo,
-            method: "GET"
-        })
-            .then(function(response) {
-                var results = response.data;
+    //     $.ajax({
+    //         url: requestURLsolo,
+    //         method: "GET"
+    //     })
+    //         .then(function(response) {
+    //             var results = response.data;
 
-                for (var i = 0; i < results.length; i++) {
-                    var weatherSoloDiv = $("<div class=\"weather\">")
-                    var cityName = results[i].name;
-                    var temperature = results[i].main.temp;
-                    var windSpeed = results [i].wind.speed;
+    //             for (var i = 0; i < results.length; i++) {
+    //                 var weatherSoloDiv = $("<div class=\"weather\">")
+    //                 var cityName = results[i].name;
+    //                 var temperature = results[i].main.temp;
+    //                 var windSpeed = results [i].wind.speed;
 
-                    var p = $("<p>").text(cityName, temperature, windSpeed)
+    //                 var p = $("<p>").text(cityName, temperature, windSpeed)
 
-                    weatherSoloDiv.append(p);
-                }
-            })
-    })
+    //                 weatherSoloDiv.append(p);
+
+    //                 $("#cities").append(weatherSoloDiv);
+    //                 console.log(results);
+    //             }
+    //         })
+    // })
     // add new city button?
     $("#add-city").on("click", function(event) {
         event.preventDefault();
 
-        var newCity = $("input").eq(0).val();
-        var newCity = $("#city-input").val();
+        // var newCity = $("input").eq(0).val();
+        // var newCity = $("#city-input").val();
+        var newCity = "Raleigh";
+
 
         if (newCity.length > 2) {
             citySearchArray.push(newCity)
@@ -71,8 +73,79 @@ $(document).ready(function() {
 
         populateElements(citySearchArray, "city-button", "#city-buttons");
     });
+
+    //main weather display====================
+    // function loadMainWeather() {
+    //     var type = $(this).attr("data-type");
+        // var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=9df12987c44ceefc6fedbd4add7a8abd'
+        //Test Link
+    //     var requestURLsolo = 'httpS://api.openweathermap.org/data/2.5/weather?q=raleigh&appid=9df12987c44ceefc6fedbd4add7a8abd'
+
+    //     $.ajax({
+    //         url: requestURLsolo,
+    //         method: "GET"
+    //     })
+    //         .then(function(response) {
+    //             var soloResults = response.data;
+    //             console.log(soloResults);
+    //             for (var i = 0; i < soloResults.length; i++) {
+    //                 var weatherSoloDiv = $("<div class=\"weather\">")
+    //                 var cityName = results[i].name;
+    //                 var temperature = results[i].main.temp;
+    //                 var windSpeed = results [i].wind.speed;
+
+    //                 var p = $("<p>").text(cityName, temperature, windSpeed)
+
+    //                 weatherSoloDiv.append(p);
+
+    //                 $("#cities").append(weatherSoloDiv);
+    //                 console.log(results);
+    //             }
+    //         })
+    // }
+    ////////////=============
+   function loadMainWeather (event){
+
+   
+    var type = $(this).attr("data-type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+          var results = response.data;
+  
+          for (var i = 0; i < results.length; i++) {
+            var animalDiv = $("<div class=\"animal-item\">");
+  
+            var rating = results[i].rating;
+  
+            var p = $("<p>").text("Rating: " + rating);
+  
+            var animated = results[i].images.fixed_height.url;
+            var still = results[i].images.fixed_height_still.url;
+  
+            var animalImage = $("<img>");
+            animalImage.attr("src", still);
+            animalImage.attr("data-still", still);
+            animalImage.attr("data-animate", animated);
+            animalImage.attr("data-state", "still");
+            animalImage.addClass("animal-image");
+  
+            animalDiv.append(p);
+            animalDiv.append(animalImage);
+  
+            $("#animals").append(animalDiv);
+          }
+        });
+    };
+    loadMainWeather();
     populateElements(citySearchArray, "city-button", "#city-buttons");
-})
+    });
+
+
 
 // function getAPI () {
 //     // console.log(requestURL5day);
